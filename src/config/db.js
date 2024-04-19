611feb381140c582f23a58b2e45e1ceac1c80f2c
PatricioGitHub1 <rojas_patricio_github@gmx.es>
2024-04-19 18:01:53 +0200

    falta probar script para insertar datos en mongo

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,5 @@
 module.exports = {
-    MONGODB_URI: 'mongodb://root:passpj03@localhost:27017/dam2-pj03?authSource=admin',
+    //MONGODB_URI: 'mongodb://root:passpj03@localhost:27017/dam2-pj03?authSource=admin',
+    MONGODB_URI: 'mongodb://elTeuUsuari:laTeuaContrasenya@localhost:8081/roscodrom?authSource=admin',
     MONGODB_URI_TEST: 'mongodb://root:passpj03@localhost:27017/dam2-pj03?authSource=admin' // Base de dades de test
 };
---
+++ src/script-dictionary.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const readline = require('readline');
+const dbConfig = require('./config/db');
+const mongoose = require('mongoose');
+
+const dictionaryPath = '../data/DISC2-LP/DISC2/DISC2-LP.txt';
+const language = "CA";
+
+// Define the Mongoose schema for posts
+const WordSchema = new mongoose.Schema({
+  word: String,
+  position: Number,
+  language: String,
+  timesUsed: Number,
+});
+
+// Compile model from schema
+const Word = mongoose.model('word' + language, WordSchema);
+
+function processWord(word) {
+  const processed = {
+    word: String(word.word),
+    position: Number(word.position),
+    language: String(word.language),
+    timesUsed: Number(word.timesUsed)
+  };
+
+  return processed;
+}
+
+async function readTXT(filePath, language) {
+    var wordList = [];
+
+    fs.access(filePath, fs.constants.F_OK, (err) => {
+        let currentLine = 1;
+        if (err) {
+            console.error('File does not exist or cannot be accessed.');
+            return;
+        }
+        
+        console.log('File exists.');
+    
+        const fileStream = fs.createReadStream(dictionaryPath);
+    
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+        });
+    
+        // Read each line, create object and push it into the array
+        rl.on('line', (line) => {
+            //console.log('Line:', line, currentLine);
+            const objWord = {word:line, position:currentLine, language:language, timesUsed:0};
+            wordList.push(objWord);
+            currentLine += 1;
+            console.log(objWord)
+        });
+    
+        // Close the readline interface when done
+        rl.on('close', () => {
+            console.log('File reading completed.');
+        });
+
+        return wordList;
+    });
+}
+
+
+async function insertWords(posts) {
+  await mongoose.connect(dbConfig.MONGODB_URI);
+  const processedWords = posts.map(processWord);
+  await Word.insertMany(processedWords);
+  await mongoose.disconnect();
+}
+
+async function main() {
+    console.log("Script to introduce txt data to mongo Dictionary collection");
+    
+    try {
+        const words = await readTXT(dictionaryPath, language);
+        await insertWords(words);
+        console.log('Posts inserted into the MongoDB database.');
+    } catch (error) {
+        console.error('Error processing XML or inserting into MongoDB:', error);
+    }
+}
+
+main();
