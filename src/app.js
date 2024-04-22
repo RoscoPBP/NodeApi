9b39cfd8086966050417ebd692e30ac5ca9cd791
root <root@roscodrom4.ieti.local>
2024-04-22 18:52:13 +0000

    fix rapido

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dbConfig = require('./config/db');
+const bodyParser = require('body-parser');
 const userRoutes = require('./api/routes/userRoutes');
 const Event = require('./api/models/event');
 const Word = require('./api/models/word');
